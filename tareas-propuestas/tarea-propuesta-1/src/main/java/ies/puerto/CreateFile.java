bbfb243c95285dc03421017e3ae10a5c14458e0d
PeterMartEsc <petermartesc@gmail.com>
2024-09-19 11:56:15 +0100

    refactor: Se refactoriza el Create File

--- tareas-propuestas/tarea-propuesta-1/src/main/java/ies/puerto/CreateFile.java
+++ tareas-propuestas/tarea-propuesta-1/src/main/java/ies/puerto/CreateFile.java
@@ -1,11 +1,14 @@
 package ies.puerto;
 
 //import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class CreateFile {
-    String mensaje = "Mensaje como parámetro";
+
+    String path = "src/main/resources/mensaje.txt";
+
 
     //Comprobar si existe el archivo
     //Si no existe crearlo
@@ -13,19 +16,30 @@ public class CreateFile {
 
     //Usar el bufferer reader
 
-    public void crearEscribirFile(){
-
-        String fileName = "mensaje.txt";
-        String encoding = "UTF-8";
-        try {
-            PrintWriter writer = new PrintWriter(fileName, encoding);
-            writer.println(mensaje);
-            writer.close();
+    public boolean crearEscribirFile(String mensaje){
+        try (FileWriter writer = new FileWriter(path, true);){
+            writer.write(mensaje+ "\n");
+            return true;
         } catch (IOException e) {
-            System.out.println("No se ha podido crear ni escribir el archivo");
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 
+
+    /*
+        public void crearEscribirFile(){
+
+            String fileName = "mensaje.txt";
+            String encoding = "UTF-8";
+            try {
+                PrintWriter writer = new PrintWriter(fileName, encoding);
+                writer.println(mensaje);
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("No se ha podido crear ni escribir el archivo");
+                e.printStackTrace();
+            }
+        }
+    */
 
 }
--- tareas-propuestas/tarea-propuesta-1/src/test/java/CreateFileTest.java
+++ tareas-propuestas/tarea-propuesta-1/src/test/java/CreateFileTest.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class CreateFileTest {
+import ies.puerto.CreateFile;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CreateFileTest {
+
+    CreateFile createFile;
+
+    String mensaje = "Mensaje como parámetro";
+
+    @BeforeEach
+    public void beforeEach() {
+        createFile = new CreateFile();
+    }
+
+    @Test
+    public void crearEscribirFileTest(){
+
+        Assertions.assertTrue(createFile.crearEscribirFile(mensaje), "No se ha creado/escrito el archivo");
+    }
 }
