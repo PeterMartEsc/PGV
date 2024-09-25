44088034907dcd73d47097b3fc67bae8dcf36f53
PeterMartEsc <petermartesc@gmail.com>
2024-09-25 17:08:44 +0100

    feat: Se realiza el ejercicio 9

--- tareas-propuestas/ejercicios-process-learning/src/main/java/ies/puerto/ejercicio9/Ejercicio9.java
+++ tareas-propuestas/ejercicios-process-learning/src/main/java/ies/puerto/ejercicio9/Ejercicio9.java
@@ -1,4 +1,31 @@
 package ies.puerto.ejercicio9;
 
+import java.io.IOException;
+
 public class Ejercicio9 {
+
+    public static void main(String[] args) {
+
+        ProcessBuilder pb = new ProcessBuilder("ping", "-t" ,"www.google.com");
+
+        try {
+            Process proceso = pb.start();
+            long tiempoInicio = System.currentTimeMillis();
+            boolean ejecutandose = true;
+
+            while(ejecutandose){
+                long tiempoActual = System.currentTimeMillis();
+                if((tiempoActual - tiempoInicio) > 10000){
+                    proceso.destroy();
+                    ejecutandose = false;
+                }
+            }
+
+            proceso.waitFor();
+
+            //System.out.println("El comando ha tardado " + tiempoEjecucion+ " milisegundos");
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
