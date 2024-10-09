5ed248df2c68b5fa4fbf0bd153304bcae5e4a98a
PeterMartEsc <petermartesc@gmail.com>
2024-10-09 17:46:16 +0100

    feat: Se trabaja en la tarea 2, monsterhunter

--- tareas-propuestas/monster-hunter-threads/src/main/java/es/ies/puerto/controlador/MonsterHunter.java
+++ tareas-propuestas/monster-hunter-threads/src/main/java/es/ies/puerto/controlador/MonsterHunter.java
@@ -1,23 +1,34 @@
-package es.ies.puerto;
+package es.ies.puerto.controlador;
 
 import es.ies.puerto.modelo.Hunter;
 import es.ies.puerto.modelo.Mapa;
 import es.ies.puerto.modelo.Monster;
 
-import java.util.HashMap;
+public class MonsterHunter implements Runnable {
 
-public class MonsterHunter {
-
+    private static Mapa mapa = new Mapa(1);
+    private final static int TIEMPO_MAXIMo = 20000;
     private final static boolean TIEMPO_AGOTADO = false;
 
+    @Override
+    public void run() {
 
 
 
-    public class JuegoCazadores {
-        public static void main(String[] args) {
-            Thread maxi = new Thread((Runnable) new Hunter());
-            Thread dragon = new Thread((Runnable) new Monster());
+    }
 
-        }
+
+
+    public static void main(String[] args) {
+
+        Hunter maxi = new Hunter(1, "Maxi",0 , 1,1);
+        Monster dragon = new Monster(1, "Dragon", 10, 10);
+
+        mapa.agregarHunter(maxi);
+        mapa.agregarMonstruo(dragon);
+
+        Thread cazador1 = new Thread((Runnable) maxi);
+        Thread monstruo1 = new Thread((Runnable) dragon);
+
     }
 }
--- tareas-propuestas/monster-hunter-threads/src/main/java/es/ies/puerto/modelo/Mapa.java
+++ tareas-propuestas/monster-hunter-threads/src/main/java/es/ies/puerto/modelo/Mapa.java
@@ -1,39 +1,94 @@
 package es.ies.puerto.modelo;
 
-public class Map {
-    private int positionX;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Random;
 
-    private int positionY;
+//Mapa contiene posiciṕon del cazador y del moinstruo
+//Metodos de colocar cazador y monstruo
+//Metodo mover cazador
+
+public class Mapa {
+
+    private int mapId;
+    private HashMap<Integer, Integer> positionsMonster;
+    private HashMap<Integer, Integer> positionsHunter;
 
 
-    public Map(int positionX, int positionY) {
-        this.positionX = positionX;
-        this.positionY = positionY;
+    public Mapa(int mapId) {
+        this.mapId = mapId;
     }
 
-    public int getPositionX() {
-        return positionX;
+    public Mapa(int mapId, HashMap<Integer, Integer> positionsMonster, HashMap<Integer, Integer> positionsHunter) {
+        this.mapId = mapId;
+        this.positionsMonster = positionsMonster;
+        this.positionsHunter = positionsHunter;
     }
 
-    public void setPositionX(int positionX) {
-        this.positionX = positionX;
+    public int getMapId() {
+        return mapId;
     }
 
-    public int getPositionY() {
-        return positionY;
+    public void setMapId(int mapId) {
+        this.mapId = mapId;
     }
 
-    public void setPositionY(int positionY) {
-        this.positionY = positionY;
+    public HashMap<Integer, Integer> getPositionsMonster() {
+        return positionsMonster;
     }
 
+    public void setPositionsMonster(HashMap<Integer, Integer> positionsMonster) {
+        this.positionsMonster = positionsMonster;
+    }
 
+    public HashMap<Integer, Integer> getPositionsHunter() {
+        return positionsHunter;
+    }
+
+    public void setPositionsHunter(HashMap<Integer, Integer> positionsHunter) {
+        this.positionsHunter = positionsHunter;
+    }
+
+    public synchronized void agregarMonstruo(Monster monster){
+
+        positionsMonster.put(monster.getPositionX(), monster.getPositionY());
+
+    }
+
+    public synchronized void agregarHunter(Hunter hunter){
+
+        positionsHunter.put(hunter.getPositionX(), hunter.getPositonY());
+
+    }
+
+    private boolean pelea(){
+        Random random = new Random();
+        int result = random.nextInt(1);
+        if(result>0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Mapa mapa = (Mapa) o;
+        return mapId == mapa.mapId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mapId);
+    }
 
     @Override
     public String toString() {
-        return "Map{" +
-                "positionX=" + positionX +
-                ", positionY=" + positionY +
+        return "Mapa{" +
+                "mapId=" + mapId +
+                ", size=" + positionsMonster +
                 '}';
     }
 }
